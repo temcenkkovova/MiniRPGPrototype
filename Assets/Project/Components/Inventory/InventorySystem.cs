4aa3fdf3b7ca0f8d600d96e9e273b5a9f775c2b8
Temchenko <temcenkovova8@gmail.com>
2026-05-29 22:36:51 +0200

    Added Add and Remove methods

--- Assets/Project/Components/Inventory/InventorySystem.cs
+++ Assets/Project/Components/Inventory/InventorySystem.cs
@@ -1,12 +1,24 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class InventorySystem : MonoBehaviour
 {
   public List<ItemData> inventoryItems;
+  public event Action OnInventoryChanged;
 
   public void AddItem(ItemData item)
   {
     inventoryItems.Add(item);
+    OnInventoryChanged?.Invoke();
+  }
+
+  public void RemoveItem(ItemData item)
+  {
+    if (inventoryItems.Contains(item))
+    {
+      inventoryItems.Remove(item);
+      OnInventoryChanged?.Invoke();
+    }
   }
 }

ebd4fbeb3a3029ac48affd4e2ffb8f492a2103bd
Temchenko <temcenkovova8@gmail.com>
2026-06-16 19:03:07 +0200

    Added fill image for exp field

--- Assets/Project/UI/Player/PlayerExpUI.cs
+++ Assets/Project/UI/Player/PlayerExpUI.cs
@@ -1,21 +1,41 @@
+
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerExpUI : MonoBehaviour
 {
   public TMP_Text textField;
   public PlayerLevel playerLevel;
+  private float delaySpeed = 1f;
+  public Image fillImage;
+  private float target;
 
   void Start()
   {
     if (playerLevel == null) return;
     playerLevel.OnCurrentExpChanged += ShowExp;
     ShowExp(playerLevel.CurrentExp);
+    fillImage.fillAmount = 0f;
+  }
+  void Update()
+  {
+    Debug.Log(target);
+    fillImage.fillAmount = Mathf.Lerp(fillImage.fillAmount, target, Time.deltaTime * delaySpeed);
+
   }
 
   public void ShowExp(float value)
   {
+
     textField.text = value.ToString("F4") + " / " + playerLevel.ExpToNextLevel.ToString("F4");
+    target = playerLevel.CurrentExp / playerLevel.ExpToNextLevel;
+    if (playerLevel.CurrentExp >= playerLevel.ExpToNextLevel)
+    {
+
+      target = 0f;
+    }
+
   }
 
   void OnDisable()

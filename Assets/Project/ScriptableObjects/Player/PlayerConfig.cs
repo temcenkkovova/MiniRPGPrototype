b6bae667f472b01a7b702ca0088882c7fb290f15
Temchenko <temcenkovova8@gmail.com>
2026-06-20 14:59:42 +0200

    Added armor to player config

--- Assets/Project/ScriptableObjects/Player/PlayerConfig.cs
+++ Assets/Project/ScriptableObjects/Player/PlayerConfig.cs
@@ -4,6 +4,7 @@ public class PlayerConfig : ScriptableObject
 {
   public float maxHealth;
   public float moveSpeed;
+  public float baseArmor;
   public float expToNextLevel;
   public float multiplyExp;
   public float baseDamage;

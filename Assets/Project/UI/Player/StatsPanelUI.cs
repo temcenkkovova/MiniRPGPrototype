4cb75dc2688022370174c5702e59a1022acb86bc
Temchenko <temcenkovova8@gmail.com>
2026-07-04 11:03:06 +0200

    The every zone has its own enemies

--- Assets/Project/UI/Player/StatsPanelUI.cs
+++ Assets/Project/UI/Player/StatsPanelUI.cs
@@ -8,14 +8,18 @@ public class StatsPanelUI : MonoBehaviour
   public TMP_Text speedField;
   public TMP_Text armorField;
   public TMP_Text totalBattlePowerField;
+  public TMP_Text weaponDamageField;
+  public TMP_Text weaponBPField;
 
   public void InitStats(PlayerStats playerStats, WeaponStats weaponStats)
   {
-    damageField.text = "Player " + playerStats.BaseDamage + "  /  " + "Weapon " + weaponStats.WeaponDamage;
+    damageField.text = playerStats.BaseDamage.ToString();
+    weaponDamageField.text = weaponStats.WeaponDamage.ToString();
     healthField.text = playerStats.Health.ToString();
     speedField.text = playerStats.MoveSpeed.ToString();
     armorField.text = "20";
-    totalBattlePowerField.text = "Player " + playerStats.PlayerPower.ToString() + "  /  " + "Weapon " + weaponStats.CombatPower.ToString();
+    totalBattlePowerField.text = playerStats.PlayerPower.ToString();
+    weaponBPField.text = weaponStats.CombatPower.ToString();
 
   }
 }
